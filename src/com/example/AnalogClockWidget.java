ad0738b8137a7a30d84efd95e4068909207d8e9d
karanbalkar <karanbalkar@gmail.com>
2013-04-13 22:31:29 +0530

    added_code

---
+++ src/com/example/AnalogClockWidget.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+
+
+public class AnalogClockWidget extends AppWidgetProvider
+{
+    
+}
